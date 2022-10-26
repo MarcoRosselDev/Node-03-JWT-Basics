447d41a19c8ec6eabaa8d53b852b7c375b577d41
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-26 05:48:55 -0300

    header authorization try catch

--- controllers/main.js
+++ controllers/main.js
@@ -24,16 +24,24 @@ const login = async (req, res) => {
 const dashboard = async (req, res) => {
   const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startWith("Bearer ")) {
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
     throw new CustomAPIError("No token provided", 401);
   }
 
-  const luckyNumber = Math.floor(Math.random() * 100);
+  const token = authHeader.split(" ")[1];
 
-  res.status(200).json({
-    msg: `Hello, marco`,
-    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
-  });
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decoded);
+    const luckyNumber = Math.floor(Math.random() * 100);
+
+    res.status(200).json({
+      msg: `Hello, ${decoded.username}`,
+      secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+    });
+  } catch (error) {
+    throw new CustomAPIError("Not authorized to access this route", 401);
+  }
 };
 
 module.exports = {
