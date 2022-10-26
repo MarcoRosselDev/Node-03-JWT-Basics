97339f6dcd387037c46619f72d5e006b6d0b4ede
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-26 06:51:41 -0300

    using http-status-code on auth 500

--- controllers/main.js
+++ controllers/main.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const CustomAPIError = require("../errors/custom-error");
+const { BadRequestError } = require("../errors");
 
 const login = async (req, res) => {
   const { username, password } = req.body;
@@ -8,7 +8,7 @@ const login = async (req, res) => {
   // check in the controller
 
   if (!username || !password) {
-    throw new CustomAPIError("Please provide email and password", 400);
+    throw new BadRequestError("Please provide email and password");
   }
 
   // just for demo, normally provided by DB!!
@@ -22,7 +22,6 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-  console.log(req.user);
   const luckyNumber = Math.floor(Math.random() * 100);
 
   res.status(200).json({
